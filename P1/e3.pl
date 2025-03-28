27a0b89c6b1c37c7a23e06ae3881437d5d874baf
hezek-02 <nery.ezequiel.capo@fing.edu.uy>
2025-03-28 18:59:30 -0300

    e3

--- P1/e3.pl
+++ P1/e3.pl
@@ -1,7 +1,30 @@
-
 nat(0).
 nat(s(X)) :- nat(X).
 
-suma(X,s(Y),s(S)) :- suma(X,Y,S).
-suma(X,0,X).
 
+suma(0,X,X).
+suma(s(X),Y,s(S)) :- suma(X,Y,S).
+
+%X-Y=R -> R+Y=X
+resta(X,Y,R) :- suma(R,Y,X).
+
+%3*4=12-> 3+3+3+3=12.
+producto(0,Y,0).
+producto(s(X), Y, P) :- producto(X, Y, W), suma(Y,W,P).
+
+distintos(s(X),0). 
+distintos(0,s(X)). 
+distintos(s(X),s(Y)) :- distintos(X,Y).
+
+mayor(s(X),0).
+mayor(s(X),s(Y)) :- mayor(X,Y).
+
+factorial(0, s(0)).  % factorial(0) = 1
+factorial(s(X), Y) :- 
+    factorial(X, Z), 
+    producto(s(X), Z, Y).  
+
+potencia(_, 0, s(0)).  % X^0 = 1
+potencia(X, s(Y), Z) :- 
+    potencia(X, Y, W), 
+    producto(X, W, Z).
