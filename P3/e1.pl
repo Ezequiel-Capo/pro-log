e2757446d0fe3341b66b98c816da0d3ae13836a4
VALENTINA CAPO BRAZEIRO <61360984@ceibal.edu.uy>
2025-05-09 21:16:37 -0300

    fix: p3e1

--- P3/e1.pl
+++ P3/e1.pl
@@ -3,7 +3,6 @@
 /* Mapa([region(a, R, [A, V]),
       region(b, A, [V, R]),
       region(c, V, [A]),
-*/
 
 colores([rojo, verde, azul]).
 mapa([region(a, rojo, [azul, verde]),
@@ -15,3 +14,15 @@ colorear([region(_Nombre, Color, Vecinos) | Resto], Colores) :-
       member(Color, Colores), 
       \+ (member(Color, Vecinos)),
       colorear(Resto, Colores).
+	  */
+
+colorear(Mapa, Colores) :- generate(Mapa, Colores), check(Mapa).
+
+not_member(_,[]).
+not_member(X, [Y|L]) :- X\=Y, not_member(X, L).
+
+generate([],_).
+generate([region(_,X,_)|L],Colores) :- member(X,Colores),generate(L,Colores).
+
+check([]).
+check([region(_,X,Vecinos)|L]):- not_member(X,Vecinos), check(L).
